c5e433ddec216ddc5523d3c255bfbbda814db6cf
sonyjunior237 <atelefackbrolin@gmail.com>
2025-11-08 13:33:00 -0500

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,6 @@
+const name = "Atelefack Brolin Jifack";
+const age = 23;
+const favouriteLanguage = "JavaScript";
+console.log('Name:' + name);
+console.log('Age:' + age);
+console.log('favorite Programming Language:' + favouriteLanguage);
